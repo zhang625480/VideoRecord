c6093fedfaf3c9174e67ba3f6355d3fb7d0b50d6
zhangzhen <zhangzhen@emagsoftware.cn>
2018-08-29 00:04:10 +0800

    branch  提交

--- app/src/main/java/com/zhang625480/video/record/Branch1.java
+++ app/src/main/java/com/zhang625480/video/record/Branch1.java
@@ -1,6 +1,13 @@
 package com.zhang625480.video.record;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 
 public class Branch1 extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
